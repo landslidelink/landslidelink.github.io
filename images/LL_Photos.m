4736c247e184f8e5c41c3c76a7a1379b64d318a0
fulmere <133609316+fulmere@users.noreply.github.com>
2025-02-10 13:04:41 -0800

    Auto update on 2025-02-10 13:04:41

--- images/LL_Photos.m
+++ images/LL_Photos.m
@@ -1,7 +1,7 @@
 
 
 
-img = imread('fcmap.png'); % Read image
+img = imread('hoosarial.png'); % Read image
 fig = figure;
 imshow(img);
 axis off; % Remove axes
@@ -13,7 +13,7 @@ set(gcf, 'Color', 'w', 'Position', [100, 100, size(img, 2), size(img, 1)]); % Ad
 
 % Define output path
 folderPath = 'C:\Users\fulmere\Documents\GitHub\landslidelink.github.io\images'; 
-fileName = fullfile(folderPath, 'fcmap.svg'); 
+fileName = fullfile(folderPath, 'hoosarial.svg'); 
 
 % Save as SVG without borders
 print(fig, fileName, '-dsvg', '-r300');
