df07868a8f6e8591d2d2240692b70713ec49deed
fulmere <133609316+fulmere@users.noreply.github.com>
2025-02-11 12:22:03 -0800

    Auto update on 2025-02-11 12:22:03

--- run_processing.ps1
+++ run_processing.ps1
@@ -2,7 +2,7 @@
 $matlabPath = "C:\Program Files\MATLAB\R2023a\bin\matlab.exe"
 
 # Define the MATLAB script path (make sure it's saved as 'processing.m')
-$scriptPath = "C:\Users\fulmere\Documents\GitHub\landslidelink.github.io\processing\processing"
+$scriptPath = "C:\Users\fulmere\Documents\GitHub\ll_processing\processing"
 
 # Run MATLAB in batch mode (no UI) and execute the script
 Start-Process -NoNewWindow -FilePath $matlabPath -ArgumentList "-batch `"run('$scriptPath'); exit;`""
