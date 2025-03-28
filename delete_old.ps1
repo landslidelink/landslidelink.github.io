37022fd60e4b63a6b3abdc11626550edb7bf596e
landslidelink <landslidelink@gmail.com>
2025-03-28 15:30:04 -0700

    Auto update on 2025-03-28 15:30:04

--- delete_old.ps1
+++ delete_old.ps1
@@ -1,8 +1,8 @@
 # Define the MATLAB executable path (update if needed)
-$matlabPath = "C:\Program Files\MATLAB\R2023a\bin\matlab.exe"
+$matlabPath = "C:\Program Files\MATLAB\R2024b\bin\matlab.exe"
 
 # Define the MATLAB script path 
-$scriptPath = "C:\Users\fulmere\Documents\GitHub\landslidelink.github.io\processing\delete_old.m"
+$scriptPath = "C:\Users\fulmere\Documents\GitHub\ll_processing\delete_old.m"
 
 # Run MATLAB in batch mode (no UI) and execute the script
 Start-Process -NoNewWindow -FilePath $matlabPath -ArgumentList "-batch `"run('$scriptPath'); exit;`""
